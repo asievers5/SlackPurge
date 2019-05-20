9b3704fc0ae61d37e82a2fed523bdbc0a19ed022
Andrew Sievers <andrew.sievers@selu.edu>
2019-05-20 17:22:33 -0500

    first commit

---
+++ SlackPurge/SlackPurge/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using RestSharp;
+using static SlackPurge.Message;
+using static SlackPurge.RootObject;
+using System.Threading;
+
+namespace SlackPurge
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            TokenEntry.Completed += (object sender, EventArgs e) =>
+            {
+                ChannelEntry.Focus();
+            };
+
+            string tokenProperty, channelProperty, userProperty;
+            if (App.Current.Properties.ContainsKey("tokenProperty"))
+            {
+                tokenProperty = App.Current.Properties["tokenProperty"] as string;
+                TokenEntry.Text = tokenProperty;
+            }
+            if (App.Current.Properties.ContainsKey("channelProperty"))
+            {
+                channelProperty = App.Current.Properties["channelProperty"] as string;
+                ChannelEntry.Text = channelProperty;
+            }
+            if (App.Current.Properties.ContainsKey("userProperty"))
+            {
+                userProperty = App.Current.Properties["userProperty"] as string;
+                UserEntry.Text = userProperty;
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            if (App.Current.Properties.ContainsKey("tokenProperty"))
+            {
+                App.Current.Properties["tokenProperty"] = TokenEntry.Text;
+                //App.Current.SavePropertiesAsync();
+            }
+            else
+            {
+                App.Current.Properties.Add("tokenProperty", TokenEntry.Text);
+            }
+            if (App.Current.Properties.ContainsKey("channelProperty"))
+            {
+                App.Current.Properties["channelProperty"] = ChannelEntry.Text;
+            }
+            else
+            {
+                App.Current.Properties.Add("channelProperty", ChannelEntry.Text);
+            }
+            if (App.Current.Properties.ContainsKey("userProperty"))
+            {
+                App.Current.Properties["userProperty"] = UserEntry.Text;
+            }
+            else
+            {
+                App.Current.Properties.Add("userProperty", UserEntry.Text);
+            }
+
+            
+            //GET messages
+            var client = new RestClient("https://slack.com/api/conversations.history?token=" + TokenEntry.Text + "&channel=" + ChannelEntry.Text + "&pretty=1");
+            var request = new RestRequest(Method.GET);            
+            IRestResponse<RootObject> response = client.Execute<RootObject>(request);
+            string value = response.Content;
+            var fetch = JsonConvert.DeserializeObject<RootObject>(value);
+
+            // Reset deleted messages text
+            ResponseText.Text = "Deleted Messages:\n";
+
+            //Delete messages and show user which messages were deleted
+            foreach (SlackPurge.Message timestamp in fetch.messages)
+            {
+                if(timestamp.user == UserEntry.Text)
+                {
+                    client = new RestClient("https://slack.com/api/chat.delete?token=" + TokenEntry.Text + "&channel=" + ChannelEntry.Text + "&ts=" + timestamp.ts + "&pretty=1");
+                    client.Execute(request);
+
+                    ResponseText.Text += timestamp.user + ": " + timestamp.text + "\n";
+                    //Thread.Sleep(1200);
+                }
+
+            }
+            Console.WriteLine(fetch.messages[0].text);
+            
+
+        }
+
+           
+    }
+}
---
+++ SlackPurge/SlackPurge/MessageModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlackPurge
+{
+    public class Message
+    {
+        public string type { get; set; }
+        public string subtype { get; set; }
+        public string text { get; set; }
+        public string user { get; set; }
+        public string ts { get; set; }
+        public string client_msg_id { get; set; }
+    }
+
+    public class RootObject
+    {
+        public bool ok { get; set; }
+        public List<Message> messages { get; set; }
+        public bool has_more { get; set; }
+        public int pin_count { get; set; }
+    }
+}
